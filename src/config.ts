e5c197b02355eb33456953bcbcb9bdfedda1ea9a
Nikhil Tikle <nikhiltikle31@gmail.com>
2023-03-20 17:41:28 +0530

    Add user interface and API base URL

---
+++ src/config.ts
@@ -0,0 +1,1 @@
+export const API_BASE_URL = 'https://give-me-users-forever.vercel.app/api/';
---
+++ src/interfaces/User.ts
@@ -0,0 +1,9 @@
+export interface User {
+  readonly ID: string;
+  JobTitle: string;
+  EmailAddress: string;
+  FirstNameLastName: string;
+  Email: string;
+  Phone: string;
+  Company: string;
+}
